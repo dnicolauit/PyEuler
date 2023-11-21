2d61f3e8311410b07ce3d94888936695d334576c
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-11-21 21:33:07 +0000

    change directories

---
+++ Unsolved/859_problem.py
@@ -0,0 +1,36 @@
+"""
+Project Euler Problem 859 - Cookie Game
+
+Answer: 
+Elapsed time (seconds): 
+
+Solved with Python 3.10.12
+""" 
+
+
+N = 7
+
+def first_even(piles):
+    for i, pile in enumerate(piles):
+        if pile % 2 == 0:
+            return i
+
+# Create a list of lists where each list contains the division of the number N into at list N
+def divide_piles(N):
+    piles = []
+
+
+    
+    return piles
+
+
+# def game(): 
+
+
+print(divide_piles(N))
+
+
+
+# piles = [5,4,9,3,1,5]
+# x = piles[first_even(piles)]
+# print(x)
