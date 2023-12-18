fc3f8d1ade785e7825332ceb91635b16f0b44ad4
Daniel Nicolau <2232623@my.ipleiria.pt>
2023-12-18 15:16:08 +0000

    problem 4

---
+++ Solved_Solutions/4_problem.py
@@ -0,0 +1,30 @@
+"""
+Project Euler Problem 4 - Largest Palindrome Product
+
+Answer: 906609
+Elapsed time (seconds): 0.25733
+
+Solved with Python 3.10.12
+Author: dnicolauit
+""" 
+
+import time
+
+def ispalindrome(number):
+    return str(number) == str(number)[::-1]
+
+def max_palindrome_product(max, min):
+    max_palindrome = 0
+    for i in range(max, min, -1):
+        for j in range(max, min, -1):
+            if ispalindrome(i*j) and i*j > max_palindrome:
+                max_palindrome = i*j
+    return max_palindrome
+
+start_time = time.time()
+solution = max_palindrome_product(999, 99)
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+print("Solution: ", solution)
+print("Elapsed time: ", elapsed_time) 
